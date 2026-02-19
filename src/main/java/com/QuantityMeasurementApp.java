6fa0973d4c7670feca094ccd9bb47d668dc91f10
nitishkumar124 <ns8231001@gmail.com>
2026-02-19 14:16:27 +0530

    [Nitish Kumar] add . submission: UC2-InchEquality

--- src/main/java/com/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp.java
@@ -24,8 +24,31 @@ public class QuantityMeasurementApp {
             return Double.compare(this.value, other.value) == 0;
         }
     }
+    
+    public static class Inches {
+        private final double value;
 
-    public static void main(String[] args) {
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+    
+    public static void demonstrateFeetEquality() {
         Feet feet1 = new Feet(1.0);
         Feet feet2 = new Feet(1.0);
         System.out.println("Comparing 1.0 ft and 1.0 ft: " + feet1.equals(feet2));
@@ -33,11 +56,20 @@ public class QuantityMeasurementApp {
         Feet feet3 = new Feet(1.0);
         Feet feet4 = new Feet(2.0);
         System.out.println("Comparing 1.0 ft and 2.0 ft: " + feet3.equals(feet4));
+    }
+    
+    public static void demonstrateInchesEquality() {
+        Inches inches1 = new Inches(1.0);
+        Inches inches2 = new Inches(1.0);
+        System.out.println("Comparing 1.0 inch and 1.0 inch: " + inches1.equals(inches2));
 
-        Feet feet5 = new Feet(1.0);
-        System.out.println("Comparing 1.0 ft and null: " + feet5.equals(null));
+        Inches inches3 = new Inches(1.0);
+        Inches inches4 = new Inches(2.0);
+        System.out.println("Comparing 1.0 inch and 2.0 inch: " + inches3.equals(inches4));
+    }
 
-        Feet feet6 = new Feet(1.0);
-        System.out.println("Comparing 1.0 ft with itself: " + feet6.equals(feet6));
+    public static void main(String[] args) {
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
     }
 }
--- src/test/java/com/QuantityMeasurementAppTest.java
+++ src/test/java/com/QuantityMeasurementAppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.QuantityMeasurementApp.Feet;
+import com.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
 
@@ -38,5 +39,38 @@ public class QuantityMeasurementAppTest {
     public void testFeetEquality_SameReference() {
         Feet feet1 = new Feet(1.0);
         assertTrue(feet1.equals(feet1));
+    }
+    
+    @Test
+    public void testInchesEquality_SameValue() {
+        Inches inches1 = new Inches(1.0);
+        Inches inches2 = new Inches(1.0);
+        assertTrue(inches1.equals(inches2));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue() {
+        Inches inches1 = new Inches(1.0);
+        Inches inches2 = new Inches(2.0);
+        assertFalse(inches1.equals(inches2));
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+        Inches inches1 = new Inches(1.0);
+        assertFalse(inches1.equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClass() {
+        Inches inches1 = new Inches(1.0);
+        String nonInchesObject = "1.0";
+        assertFalse(inches1.equals(nonInchesObject));
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+        Inches inches1 = new Inches(1.0);
+        assertTrue(inches1.equals(inches1));
     }
 }
